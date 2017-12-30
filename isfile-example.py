b0365403dabffc0ce3e56c9d59368f171154482f
vladimir-zolotykh <vladimir.zolotykh@gmail.com>
2017-12-30 14:39:44 +0200

    Initial add
    How to check if the regular file exists.

---
+++ isfile-example.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+#import subprocess
+
+import os
+path = "words2.txt"
+#os.path.exists(path) # returns whether the path (dir or file) exists or not
+if os.path.isfile(path) :
+    print "The file %s exists" % path
