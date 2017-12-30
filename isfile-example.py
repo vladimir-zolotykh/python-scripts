fe5b8f7774b8384863065dcba55b5f16ba342f9c
vladimir-zolotykh <vladimir.zolotykh@gmail.com>
2017-12-30 14:59:27 +0200

    Add directory check

--- isfile-example.py
+++ isfile-example.py
@@ -6,3 +6,6 @@ path = "words2.txt"
 #os.path.exists(path) # returns whether the path (dir or file) exists or not
 if os.path.isfile(path) :
     print "The file %s exists" % path
+dirpath = "foodir"
+if os.path.isdir(dirpath):
+    print "The file %s is a directory." % dirpath
