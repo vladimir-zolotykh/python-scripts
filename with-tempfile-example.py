016e05a5a0ce3a551d3a6bf6da787c69ff63b8ba
vladimir-zolotykh <vladimir.zolotykh@gmail.com>
2018-01-01 15:36:08 +0200

    Initial add
    Use Bash select function from within the Python code.

---
+++ with-tempfile-example.py
@@ -0,0 +1,27 @@
+
+import tempfile
+import subprocess
+
+select_target_sh_func = """
+#!/bin/bash
+function select_target {
+    target_list=(%s)
+    PS3="Select Target: "
+    select target in "${target_list[@]}"; do
+ 	    break
+    done
+    echo $target
+}
+select_target
+"""
+
+target_list = ["Target1", "Target2", "Target3"]
+
+with tempfile.NamedTemporaryFile() as temp:
+    temp.write(select_target_sh_func % ' '.join(map(lambda s : '\"%s\"' % str(s),target_list)))
+    subprocess.call(['chmod', '0777', temp.name])
+
+    sh_proc = subprocess.Popen(["bash", temp.name], stdout=subprocess.PIPE)
+    (output, err) = sh_proc.communicate()
+    exit_code = sh_proc.wait()
+    print output
