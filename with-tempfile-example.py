58d6da0af313ec7e91dd6a95eed9cdd73b316157
vladimir-zolotykh <vladimir.zolotykh@gmail.com>
2018-01-01 19:15:38 +0200

    Add temp.flush(), fix stdout=
    Now it words as expected.

--- with-tempfile-example.py
+++ with-tempfile-example.py
@@ -1,7 +1,7 @@
 
 import tempfile
 import subprocess
-
+import os
 select_target_sh_func = """
 #!/bin/bash
 function select_target {
@@ -19,9 +19,10 @@ target_list = ["Target1", "Target2", "Target3"]
 
 with tempfile.NamedTemporaryFile() as temp:
     temp.write(select_target_sh_func % ' '.join(map(lambda s : '\"%s\"' % str(s),target_list)))
+    temp.flush()
+    # bash: /var/folders/jm/4j4mq_w52bx2l5qwg4gt44580000gn/T/tmp00laDV: Permission denied
     subprocess.call(['chmod', '0777', temp.name])
-
-    sh_proc = subprocess.Popen(["bash", temp.name], stdout=subprocess.PIPE)
+    sh_proc = subprocess.Popen(["bash", "-c", temp.name], stdout=subprocess.PIPE)
     (output, err) = sh_proc.communicate()
     exit_code = sh_proc.wait()
     print output
