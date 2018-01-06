0e73a52bddb95d07e253d9a572745a18d553eca4
vladimir-zolotykh <vladimir.zolotykh@gmail.com>
2018-01-06 13:20:24 +0200

    Initial add
    Output enumerated choices as a table. Ask the user to select.

---
+++ list-as-table-example.py
@@ -0,0 +1,40 @@
+
+import os
+
+path="."
+files=dict((i, f) for i,f in
+    enumerate(f for f in os.listdir(path) if f.endswith(('.py', '.txt'))))
+
+# print files
+l = files.items()
+l = ["{: >3} {:<}".format(k,v) for k,v in sorted(files.items())]
+
+# l = ['exiv2-devel', 'mingw-libs', 'tcltk-demos', 'fcgi', 'netcdf',
+#     'pdcurses-devel',     'msvcrt', 'gdal-grass', 'iconv', 'qgis-devel',
+#     'qgis1.1', 'php_mapscript']
+
+foolist = l
+
+
+
+choice = None
+while choice is None:
+    for a, b, c in zip(foolist[::3], foolist[1::3], foolist[2::3]):
+        print '{:<30}{:<30}{:<}'.format(a, b, c)
+    choice = files.get(int(raw_input('Enter selection: ')))
+    if not choice:
+        print 'Please make a valid selection'
+    else:
+        print "You selected {:s}".format(choice)
+
+
+#
+# if len(l) % 2 != 0:
+#     l.append(" ")
+#
+# split = len(l)/2
+# l1 = l[0:split]
+# l2 = l[split:]
+# for key, value in zip(l1,l2):
+# #    print '%-20s %s' % (key, value) )      #python <2.6
+#    print "{0:<20s}      {1}".format(key, value) #python 2.6+
