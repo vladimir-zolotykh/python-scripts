486d2deb7b0920cb6062b55ae633d49def48129a
vladimir-zolotykh <vladimir.zolotykh@gmail.com>
2018-01-06 21:51:47 +0200

    Introduce pselect function
    Rework using pselect function.

--- list-as-table-example.py
+++ list-as-table-example.py
@@ -1,31 +1,53 @@
 
 import os
 
+
+def pselect(choices):
+    """Ask a user to select one of the enumerated choices. Return the selection."""
+    enumerated_choices = dict((i, f) for i, f in enumerate(f for f in choices))
+    str_choices=["{: >3} {:<}".format(k,v) for k,v in sorted(enumerated_choices.items())]
+    choice = None
+    while choice is None:
+        for a, b, c in zip(str_choices[::3], str_choices[1::3], str_choices[2::3]):
+            print '{:<30}{:<30}{:<}'.format(a, b, c)
+        choice = enumerated_choices.get(int(raw_input("Selection? ")))
+        if not choice:
+            print 'Please make a valid selection'
+        else:
+            print 'You selected {:s}'.format(choice)
+
+    return (choice)
+
 path="."
-files=dict((i, f) for i,f in
-    enumerate(f for f in os.listdir(path) if f.endswith(('.py', '.txt'))))
+print pselect(os.listdir(path))
+
+
+# files=dict((i, f) for i,f in
+#     enumerate(f for f in os.listdir(path) if f.endswith(('.py', '.txt'))))
 
 # print files
-l = files.items()
-l = ["{: >3} {:<}".format(k,v) for k,v in sorted(files.items())]
+# l = files.items()
+# l = ["{: >3} {:<}".format(k,v) for k,v in sorted(files.items())]
 
 # l = ['exiv2-devel', 'mingw-libs', 'tcltk-demos', 'fcgi', 'netcdf',
 #     'pdcurses-devel',     'msvcrt', 'gdal-grass', 'iconv', 'qgis-devel',
 #     'qgis1.1', 'php_mapscript']
 
-foolist = l
+#foolist = l
 
 
 
-choice = None
-while choice is None:
-    for a, b, c in zip(foolist[::3], foolist[1::3], foolist[2::3]):
-        print '{:<30}{:<30}{:<}'.format(a, b, c)
-    choice = files.get(int(raw_input('Enter selection: ')))
-    if not choice:
-        print 'Please make a valid selection'
-    else:
-        print "You selected {:s}".format(choice)
+# choice = None
+# while choice is None:
+#     for a, b, c in zip(foolist[::3], foolist[1::3], foolist[2::3]):
+#         print '{:<30}{:<30}{:<}'.format(a, b, c)
+#     choice = files.get(int(raw_input('Enter selection: ')))
+#     if not choice:
+#         print 'Please make a valid selection'
+#     else:
+#         print "You selected {:s}".format(choice)
+
+
 
 
 #
