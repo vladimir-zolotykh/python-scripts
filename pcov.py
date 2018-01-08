e6cd473cec1dc71f9dc257e12b844d2b322ba343
vladimir-zolotykh <vladimir.zolotykh@gmail.com>
2018-01-08 15:54:35 +0200

    Add some conf paths like COV_INTDIR

--- pcov.py
+++ pcov.py
@@ -1,3 +1,5 @@
+import os
+
 import subprocess
 
 class Cov0:
@@ -5,6 +7,7 @@ class Cov0:
     COV_HTMLDIR=""
     BUILD_CMD=""
     COV_CA_COND=""
+    BUNNY_BIN=""
     COV_BIN='/opt/cov-analysis-linux-8.5.0/bin'
     def exec_cmd (self, cmd):
         ret = subprocess.call(cmd, shell=True)
@@ -14,6 +17,12 @@ class Cov0:
         return ret
     
 class Cov(Cov0) :
+    def __init__(self, cwd):
+        self.COV_INTDIR=os.path.join(cwd, "/coverity/intdir")
+        self.COV_HTMLDIR=os.path.join(cwd, "/coverity/report")
+        self.BUNNY_BIN=os.path.join(cwd, "/Tools/Bunny/Bunny/bin")
+        #COV_CA_CONF=$COV_BIN/visteon_Ruleset_v4_0.json
+        self.COV_CA_CONF=os.path.join(self.COV_BIN, "/visteon_Ruleset_v4_0.json")
     def clean_space(self):
         super(Cov).clean0('rm -rf ProductSpace/* InterSpace/*')
     def clean_intdir(self):
@@ -39,3 +48,8 @@ class Cov(Cov0) :
         cmd='{:s}/cov-format-errors --dir {:s} --html-output {:s} --noX --exclude-files ".*/.h'\
             .format(self.COV_BIN, self.COV_INTDIR, self.COV_HTMLDIR)
         super(Cov, self).exec_cmd(cmd)
+
+if __name__ == "__main__" :
+    cwd = os.getcwd()
+
+    Cov2 = Cov(cwd)
